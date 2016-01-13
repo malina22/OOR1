aac2c0a4e7202281e73b4384d9a2c0d51deee833
Kamil <m.kowalski767@wp.pl>
2016-01-13 19:43:08 +0100

    OOR1

---
+++ ConsoleApplication9/ConsoleApplication9/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading;
+
+public class Test
+{
+    static void Main()
+    {
+        Counter foo = new Counter();
+        ThreadStart job = new ThreadStart(foo.Count);
+        Thread thread = new Thread(job);
+        thread.Start();
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Main thread: {0}", i);
+            Thread.Sleep(1000);
+        }
+    }
+}
+
+public class Counter
+{
+    public void Count()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine("Other thread: {0}", i);
+            Thread.Sleep(500);
+        }
+    }
+}
